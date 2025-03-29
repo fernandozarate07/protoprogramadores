e176a80ceb9cb829c8114520fce6cb2f26410fc8
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-03-28 21:07:49 -0300

    It is added by loading to implement in the future.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import "./globals.css";
-import Link from "next/link";
 import { Questrial } from "next/font/google";
 import Nav from "./nav/nav";
-// import { FaHome, FaCode, FaSlackHash } from "react-icons/fa";
 
 export const metadata = {
   title: "Protoprogramadores",
@@ -13,6 +10,7 @@ const questrial = Questrial({
   subsets: ["latin"],
   weight: ["400"],
 });
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={questrial.className}>
